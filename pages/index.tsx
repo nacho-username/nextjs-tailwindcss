e8ce41c9939c54403a9ff82657c1ebeb71520eef
Murray Williams <murray@waterfalldigital.co.za>
2023-03-06 19:34:32 +0200

    Adds mobile adjustments to hero copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,11 @@ const Home = ({ homepage, error }: { homepage: any; error: any }) => {
     >
       <main className='bg-hero-image bg-no-repeat bg-cover bg-center h-full'>
         <div className='container mx-auto flex w-full items-center h-full'>
-          <div className='w-1/2 pt-36 text-white'>
-            <h1 className='text-7xl font-extrabold capitalize'>{hero.title}</h1>
-            <p className='text-2xl font-light text-md py-4 pb-8 leading-10'>
+          <div className='w-[90%] md:w-[60%] pt-8 md:pt-28 text-white px-4'>
+            <h1 className='text-5xl md:text-8xl font-extrabold capitalize'>
+              {hero.title}
+            </h1>
+            <p className='text-md md:text-2xl font-light text-md py-4 pb-8 leading-8 md:leading-10'>
               {hero.description}
             </p>
             <BorderButton label="let's connect" href='contact' />
