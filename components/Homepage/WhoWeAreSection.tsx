c52ac47286619b9fe8645aa63eee895f8c65189f
Murray Williams <murray@waterfalldigital.co.za>
2023-03-06 21:52:35 +0200

    Adds more bottom padding

--- components/Homepage/WhoWeAreSection.tsx
+++ components/Homepage/WhoWeAreSection.tsx
@@ -12,7 +12,7 @@ export default function WhoWeAreSection({
 }) {
   const { title, description } = whoWeAreData
   return (
-    <section className='bg-dark-grey text-center text-white pt-12 md:pt-24 pb-32 md:pb-56'>
+    <section className='bg-dark-grey text-center text-white pt-12 md:pt-24 pb-32 md:pb-64'>
       <div className='w-16 h-1 mx-auto border-0 bg-green-300 bg-gradient-to-r from-green-300 to-green-900 mb-4'></div>
       <h3 className='uppercase text-sm'>{title}</h3>
       <p className='w-[80%] md:w-1/2 mx-auto text-lg md:text-2xl font-medium pt-4 md:pt-8 leading-7 md:leading-10'>
