913e695c065272d1d22e23b05d0eca8ec7143145
Murray Williams <murray@waterfalldigital.co.za>
2023-03-06 19:47:20 +0200

    Mobile adjustments to header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,10 +2,8 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div className='absolute top-8 left-16'>
-      <div className='container mx-auto'>
-        <Image src='/images/logo.svg' alt='Logo' width={80} height={100} />
-      </div>
+    <div className='h-0 sticky top-4 container mx-auto bg-slate-900'>
+      <Image src='/images/logo.svg' alt='Logo' width={80} height={100} />
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home = ({ homepage, error }: { homepage: any; error: any }) => {
       }}
     >
       <main className='bg-hero-image bg-no-repeat bg-cover bg-center h-full'>
-        <div className='container mx-auto flex w-full items-center h-full'>
+        <div className='container mx-auto flex flex-wrap w-full items-center h-full'>
           <div className='w-[90%] md:w-[60%] pt-8 md:pt-28 text-white px-4'>
             <h1 className='text-5xl md:text-8xl font-extrabold capitalize'>
               {hero.title}
