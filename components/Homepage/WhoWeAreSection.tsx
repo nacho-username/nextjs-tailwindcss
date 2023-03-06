8bc2c2ca622e1dfdfbbf403b6e3e97ad94dea4c9
Murray Williams <murray@waterfalldigital.co.za>
2023-03-06 20:38:29 +0200

    Minor who we are adjustments

--- components/Homepage/WhoWeAreSection.tsx
+++ components/Homepage/WhoWeAreSection.tsx
@@ -17,15 +17,15 @@ export default function WhoWeAreSection({
 }) {
   console.log(headerWhoWeAre)
   return (
-    <section className='bg-dark-grey text-center text-white pt-24 pb-56'>
+    <section className='bg-dark-grey text-center text-white pt-12 md:pt-24 pb-32 md:pb-56'>
       <div className='w-16 h-1 mx-auto border-0 bg-green-300 bg-gradient-to-r from-green-300 to-green-900 mb-4'></div>
       <h3 className='uppercase text-sm'>Who we are</h3>
-      <p className='w-1/2 mx-auto text-2xl font-medium pt-8 leading-10'>
+      <p className='w-[80%] md:w-1/2 mx-auto text-lg md:text-2xl font-medium pt-4 md:pt-8 leading-7 md:leading-10'>
         Through navigating unexpected paths is when the most innovative and
         effective solutions emerge.
       </p>
       <Image
-        className='mx-auto pt-16'
+        className='mx-auto pt-8 md:pt-16 px-8'
         src='/images/problem-path.svg'
         alt=''
         width={1024}
