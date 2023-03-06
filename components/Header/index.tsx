12e991ed7da6070df83405ea7d47af53bc504905
Murray Williams <murray@waterfalldigital.co.za>
2023-03-06 17:02:12 +0200

    Adds absolute header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,11 @@
+import Image from 'next/image'
+
 export default function Header() {
-  return <div>Header component</div>
+  return (
+    <div className='absolute top-8 left-12'>
+      <div className='container mx-auto'>
+        <Image src='/images/logo.svg' alt='Logo' width={100} height={100} />
+      </div>
+    </div>
+  )
 }
