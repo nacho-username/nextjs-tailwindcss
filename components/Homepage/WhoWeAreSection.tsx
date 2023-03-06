d4ce1ef397c29733e67a577666b9dd8295eb2989
Murray Williams <murray@waterfalldigital.co.za>
2023-03-06 20:08:27 +0200

    Adds who we are component

---
+++ components/Homepage/WhoWeAreSection.tsx
@@ -0,0 +1,12 @@
+export default function WhoWeAreSection() {
+  return (
+    <section className='bg-gray-900 text-center text-white pt-24'>
+      <div className='w-24 h-1 mx-auto border-0 bg-green-300'></div>
+      <h3 className='uppercase text-sm '>Who we are</h3>
+      <p className='w-1/2 mx-auto'>
+        Through navigating unexpected paths is when the most innovative and
+        effective solutions emerge.
+      </p>
+    </section>
+  )
+}
