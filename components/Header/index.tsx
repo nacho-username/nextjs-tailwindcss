63677307a4bfcba30dd037936ad34497a91e5ce5
Murray Williams <murray@waterfalldigital.co.za>
2023-03-06 19:28:15 +0200

    Minor positioning of logo

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div className='absolute top-8 left-12'>
+    <div className='absolute top-8 left-16'>
       <div className='container mx-auto'>
-        <Image src='/images/logo.svg' alt='Logo' width={100} height={100} />
+        <Image src='/images/logo.svg' alt='Logo' width={80} height={100} />
       </div>
     </div>
   )
