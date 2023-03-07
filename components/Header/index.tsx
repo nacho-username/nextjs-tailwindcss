372f5de6d4ac00689ed3a6209b63782ea262aea5
Murray Williams <murray@waterfalldigital.co.za>
2023-03-07 16:48:06 +0200

    Adds scroll detection + header style adjustments

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,22 @@
 import Image from 'next/image'
+import { useScrollDirection } from '../../utils/scrollHook'
 
 export default function Header() {
+  const scrollDirection = useScrollDirection()
+
   return (
-    <div className='p-4 -mb-36 md:mb-0 md:h-0 md:sticky md:top-4 container mx-auto'>
-      <Image src='/images/logo.svg' alt='Logo' width={80} height={100} />
+    <div
+      className={`p-4 -mb-36 md:sticky ${
+        scrollDirection === 'down' ? 'md:-top-28' : 'md:top-0'
+      } backdrop-blur bg-deep-purple/30 rounded-lg md:mb-0 h-28 container mx-auto transition-all duration-300`}
+    >
+      <Image
+        className='pb-4'
+        src='/images/logo.svg'
+        alt='Logo'
+        width={60}
+        height={100}
+      />
     </div>
   )
 }
