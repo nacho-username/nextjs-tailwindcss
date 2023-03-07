71521e7c01537a9f00599dcc7d3eac174aa449b6
Murray Williams <murray@waterfalldigital.co.za>
2023-03-07 16:50:30 +0200

    Adjust logo position

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,9 +6,9 @@ export default function Header() {
 
   return (
     <div
-      className={`p-4 -mb-36 md:sticky ${
+      className={`py-4 px-2 -mb-20 md:sticky ${
         scrollDirection === 'down' ? 'md:-top-28' : 'md:top-0'
-      } backdrop-blur bg-deep-purple/30 rounded-lg md:mb-0 h-28 container mx-auto transition-all duration-300`}
+      } backdrop-blur bg-deep-purple/30 rounded-lg md:mb-0 h-20 container mx-auto transition-all duration-300`}
     >
       <Image
         className='pb-4'
