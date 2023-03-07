8cb7f575888a2a6d0eb6f224512e2f634fded5f5
Murray Williams <murray@waterfalldigital.co.za>
2023-03-07 10:35:23 +0200

    Adjusts mobile logo placement

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div className='h-0 sticky top-4 container mx-auto bg-slate-900'>
+    <div className='p-4 -mb-36 md:mb-0 md:h-0 md:sticky md:top-4 container mx-auto bg-dark-grey'>
       <Image src='/images/logo.svg' alt='Logo' width={80} height={100} />
     </div>
   )
