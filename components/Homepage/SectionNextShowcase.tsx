52656649c6ce601495eeffd6e98b4a7e37846bf2
Murray Williams <murray@waterfalldigital.co.za>
2023-03-08 09:38:50 +0200

    Adds new showcase section

--- components/Homepage/SectionNextShowcase.tsx
+++ components/Homepage/SectionNextShowcase.tsx
@@ -1,16 +1,77 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { SolidButton } from '../Buttons/SolidButton'
+
 interface nextShowcaseProps {
   title: string
 }
 
+interface showcaseCardProps {
+  width: string
+  image: any
+  titleHover: string
+  titleDescription: string
+  websiteLink: string
+}
+
 export default function SectionNextShowcase({
+  nextShowcaseHeader,
   nextShowcaseData,
 }: {
-  nextShowcaseData: nextShowcaseProps
+  nextShowcaseData: showcaseCardProps[]
+  nextShowcaseHeader: nextShowcaseProps
 }) {
-  const { title } = nextShowcaseData
+  const { title } = nextShowcaseHeader
+  console.log(nextShowcaseData[0].image.data.attributes.url)
   return (
-    <div className='bg-beige text-center px-16 text-deep-grey'>
-      <h3 className='w-[80%] md:w-2/3 mx-auto uppercase text-4xl'>{title}</h3>
-    </div>
+    <section className='bg-beige text-center pt-8 pb-16 px-16 text-deep-grey'>
+      <h3 className='w-[80%] md:w-2/3 mx-auto uppercase text-4xl font-bold'>
+        {title}
+      </h3>
+      <div className='grid grid-cols-1 md:grid-cols-4 gap-4 pt-8 pb-12'>
+        {nextShowcaseData.map((showcase, index) => (
+          <div
+            key={index}
+            className={`flex flex-col items-start justify-start text-left bg-white   cursor-pointer ${
+              showcase.width === 'two/thirds' ? 'col-span-2' : 'col-span-1'
+            }`}
+          >
+            <div className='relative'>
+              <Image
+                src={`http://localhost:1337${showcase.image.data.attributes.url}`}
+                alt={showcase.image.data.attributes.alternativeText}
+                width={showcase.image.data.attributes.width}
+                height={showcase.image.data.attributes.height}
+              />
+              <Link
+                className='text-lg pt-4 md:pt-8 group-hover:text-white'
+                href={showcase.websiteLink}
+                target='_blank'
+              >
+                <div className='opacity-0 hover:opacity-100 duration-300 bg-white m-8 p-4 absolute inset-0 flex flex-col items-center justify-center text-center text-dark-grey'>
+                  <h4 className='text-xl font-bold uppercase '>
+                    {showcase.titleHover}
+                  </h4>
+                  <p className='text-sm font-light'>
+                    {showcase.titleDescription}
+                  </p>
+
+                  <SolidButton
+                    classes='bg-deep-purple text-white mt-4 py-2 px-6'
+                    label='view site'
+                    href={showcase.websiteLink}
+                  />
+                </div>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+      <SolidButton
+        classes='bg-gradient-to-tr from-teal-300 to-teal-900 text-white hover:bg-gradient-to-tl hover:from-teal-400 hover:to-teal-800 py-4 px-8'
+        label='let’s build something great'
+        href='contact'
+      />
+    </section>
   )
 }
