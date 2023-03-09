f149216caecb4fe0bdc0a3891ac24b630bb22734
Murray Williams <murray@waterfalldigital.co.za>
2023-03-09 18:05:38 +0200

    Updates header spacing

--- components/Shared/Header.tsx
+++ components/Shared/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 export default function Header({ title, description, dark }: HeaderProps) {
   return (
     <>
-      <div className='w-16 h-1 mx-auto border-0  bg-gradient-to-r from-teal-300 to-teal-900 mb-4'></div>
+      <div className='w-16 h-1 mx-auto border-0  bg-gradient-to-r from-teal-300 to-teal-900 mb-2'></div>
       <h3
         className={`uppercase text-sm ${
           dark ? 'text-dark-grey' : 'text-white'
@@ -16,7 +16,7 @@ export default function Header({ title, description, dark }: HeaderProps) {
         {title}
       </h3>
       <p
-        className={`w-[80%] md:w-1/2 mx-auto text-lg md:text-3xl font-bold pt-4 leading-7 md:leading-10 ${
+        className={`w-[80%] md:w-1/2 mx-auto text-lg md:text-3xl font-bold leading-7 md:leading-10 ${
           dark ? 'text-dark-grey' : 'text-white'
         }`}
       >
