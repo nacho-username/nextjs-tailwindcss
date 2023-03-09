237dbfba497905ed1abf7d7a744633f457050c5a
Murray Williams <murray@waterfalldigital.co.za>
2023-03-09 17:49:51 +0200

    Updates service cards

--- components/Homepage/SectionServices.tsx
+++ components/Homepage/SectionServices.tsx
@@ -24,13 +24,13 @@ export default function SectionServices({
 }) {
   const { title, description } = serviceHeader
   return (
-    <section className='bg-deep-purple text-center text-white pt-16 pb-24'>
+    <section className='bg-beige text-center text-white pt-12 pb-24'>
       <div className='w-16 h-1 mx-auto border-0 bg-green-300 bg-gradient-to-r from-green-300 to-green-900 mb-4'></div>
-      <h3 className='uppercase text-sm'>{title}</h3>
-      <p className='w-[80%] md:w-1/2 mx-auto text-lg md:text-2xl font-medium pt-0 leading-7 md:leading-10'>
+      <h3 className='uppercase text-sm text-dark-grey'>{title}</h3>
+      <p className='w-[80%] md:w-1/2 mx-auto text-lg md:text-4xl font-bold pt-0 leading-7 md:leading-10 text-dark-grey'>
         {description}
       </p>
-      <div className='w-[900px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 pt-8 pb-12'>
+      <div className='w-[900px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 pt-12 pb-12'>
         {servicesList.map((service, index) => (
           <ServiceItem key={index} serviceItem={service} />
         ))}
--- components/Homepage/ServiceItem.tsx
+++ components/Homepage/ServiceItem.tsx
@@ -18,17 +18,17 @@ export default function ServiceItem({
   const { title, description } = serviceItem
   const { url, width, height } = serviceItem.icon.data.attributes
   return (
-    <div className='flex w-full p-8 justify-start items-start border-2 border-light-purple rounded-lg group hover:bg-white hover:border-white hover:cursor-cell'>
+    <div className='flex w-full p-12 justify-start items-start bg-white rounded-lg group hover:bg-dark-grey hover:border-white hover:cursor-cell'>
       <div className='w-32'>
         <Image
-          className='shadow-lg rounded-full'
+          className='rounded-full'
           src={`http://localhost:1337${url}`}
           width={width}
           height={height}
           alt=''
         />
       </div>
-      <div className='text-left ml-4 group-hover:text-dark-grey'>
+      <div className='text-left ml-4 text-dark-grey group-hover:text-white'>
         <h4 className='text-xl font-bold uppercase '>{title}</h4>
         <p className='text-sm pt-2 leading-5 font-light'>{description}</p>
       </div>
