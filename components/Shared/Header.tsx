8895667c40cfefb31ff72965dfa9cbcb8b3d80a6
Murray Williams <murray@waterfalldigital.co.za>
2023-03-10 11:45:34 +0200

    Updates header to accept classes prop

--- components/Shared/Header.tsx
+++ components/Shared/Header.tsx
@@ -2,12 +2,20 @@ interface HeaderProps {
   title: string
   description?: string
   dark?: boolean
+  classes?: string
 }
 
-export default function Header({ title, description, dark }: HeaderProps) {
+export default function Header({
+  title,
+  description,
+  dark,
+  classes,
+}: HeaderProps) {
   return (
     <>
-      <div className='w-16 h-1 mx-auto border-0  bg-gradient-to-r from-teal-300 to-teal-900 mb-2'></div>
+      <div
+        className={`w-16 h-1 border-0 bg-gradient-to-r from-teal-300 to-teal-900 mb-2 ${classes}`}
+      ></div>
       <h3
         className={`uppercase text-sm ${
           dark ? 'text-dark-grey' : 'text-white'
@@ -16,7 +24,7 @@ export default function Header({ title, description, dark }: HeaderProps) {
         {title}
       </h3>
       <p
-        className={`w-[80%] md:w-1/2 mx-auto text-lg md:text-3xl font-bold leading-7 md:leading-10 ${
+        className={`w-[80%] md:w-1/2 text-lg md:text-3xl font-bold leading-7 md:leading-10 ${
           dark ? 'text-dark-grey' : 'text-white'
         }`}
       >
