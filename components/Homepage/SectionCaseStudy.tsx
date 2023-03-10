89680fc5d797b7ed997738f4479b63a96b72bfc3
Murray Williams <murray@waterfalldigital.co.za>
2023-03-10 14:11:57 +0200

    Updates case study section

--- components/Homepage/SectionCaseStudy.tsx
+++ components/Homepage/SectionCaseStudy.tsx
@@ -1,14 +1,23 @@
+import Image from 'next/image'
 import { BorderButton } from '../Shared/BorderButton'
 import Header from '../Shared/Header'
 
 export default function SectionCaseStudy() {
   return (
-    <section>
-      <div className="flex flex-wrap bg-deep-purple min-h-500 justify-start p-4 bg-[url('/images/fibre-phoenix-case-study-bird.svg')] bg-no-repeat bg-right">
-        <div className="w-1/2 bg-[url('/images/fibre-phoenix-case-study.png')] bg-no-repeat bg-contain bg-center"></div>
-        <div className='w-1/2 self-start text-left py-8 pr-36'>
+    <section id='case-study'>
+      <div className="flex flex-wrap bg-deep-purple min-h-900 py-24 justify-center p-4 bg-[url('/images/fibre-phoenix-case-study-bird.svg')] bg-no-repeat bg-right">
+        <div className='w-7/12'>
+          <Image
+            src={'/images/fibre-phoenix-case-study.png'}
+            width={600}
+            height={600}
+            alt='Fibre Phoenix Case Study'
+            className='mx-auto'
+          />
+        </div>
+        <div className='w-3/12 self-start text-left py-8'>
           <Header title='case study' description='Fibre Phoenix' />
-          <p className='w-[60%] text-sm font-light leading-8 text-white py-4 mb-8'>
+          <p className='text-sm font-light leading-8 text-white py-4 mb-8'>
             Discover how the Fibre Phoenix web application helped customers find
             the best deals for fibre internet connections. Learn how our web
             agency team created a user-friendly interface, integrated with ISP's
