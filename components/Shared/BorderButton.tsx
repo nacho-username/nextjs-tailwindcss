ee73f4be22989e25bdc872d6a63c1a23841a5333
Murray Williams <murray@waterfalldigital.co.za>
2023-03-10 14:23:00 +0200

    Updates button styles

--- components/Shared/BorderButton.tsx
+++ components/Shared/BorderButton.tsx
@@ -17,9 +17,14 @@ export const BorderButton: React.FC<Props> = ({
   return (
     <Link
       href={`/${href}`}
-      className={`border-2 border-teal-300 py-3 px-8 rounded-tl-lg rounded-br-lg font-bold text-xs uppercase hover:bg-teal-300 inline-block transition-scale ease-in delay-75 hover:scale-95 ${classes}`}
+      className={`border-2 w-48 border-teal-300 py-3 px-8 rounded-tl-lg rounded-br-lg font-bold text-xs uppercase  inline-block relative overflow-hidden group ${classes}`}
     >
-      {label}
+      <div className='absolute inset-0 w-0 bg-green transition-all duration-[250ms] ease-out group-hover:w-full'></div>
+      <div className='relative group-hover:text-dark-grey text-center'>
+        {label}
+      </div>
     </Link>
   )
+}
+{
 }
--- components/Shared/SolidButton.tsx
+++ components/Shared/SolidButton.tsx
@@ -14,10 +14,6 @@ export const SolidButton: React.FC<Props> = ({
   classes,
 }) => {
   return (
-    //   <button class="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
-    //   <div class="absolute inset-0 w-3 bg-amber-400 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
-    //   <span class="relative text-black group-hover:text-white">Hover me!</span>
-    // </button>
     <Link
       href={`/${href}`}
       className={`w-48 rounded-tl-lg rounded-br-lg font-bold text-xs uppercase inline-block relative overflow-hidden group ${classes}`}
