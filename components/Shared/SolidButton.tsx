f09e225bdbf5248cbdfc3f2dbbaceff7371a16b6
Murray Williams <murray@waterfalldigital.co.za>
2023-03-10 13:54:32 +0200

    Updates button alignment

--- components/Shared/SolidButton.tsx
+++ components/Shared/SolidButton.tsx
@@ -23,7 +23,7 @@ export const SolidButton: React.FC<Props> = ({
       className={`w-48 rounded-tl-lg rounded-br-lg font-bold text-xs uppercase inline-block relative overflow-hidden group ${classes}`}
     >
       <div className='absolute inset-0 w-0 bg-green transition-all duration-[250ms] ease-out group-hover:w-full'></div>
-      <div className='relative group-hover:text-white'>{label}</div>
+      <div className='relative group-hover:text-white text-center'>{label}</div>
     </Link>
   )
 }
