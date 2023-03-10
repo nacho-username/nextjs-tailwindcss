128933765f1d7ad39ddec2915082849d77c7cae6
Murray Williams <murray@waterfalldigital.co.za>
2023-03-10 11:45:23 +0200

    Updates border button to accept class props

--- components/Shared/BorderButton.tsx
+++ components/Shared/BorderButton.tsx
@@ -5,13 +5,19 @@ interface Props {
   label: string
   href: string
   icon?: string
+  classes?: string
 }
 
-export const BorderButton: React.FC<Props> = ({ label, href, icon }) => {
+export const BorderButton: React.FC<Props> = ({
+  label,
+  href,
+  icon,
+  classes,
+}) => {
   return (
     <Link
       href={`/${href}`}
-      className='border-2 border-teal-300 py-3 px-8 rounded-tl-lg rounded-br-lg font-bold text-xs uppercase hover:bg-teal-300 hover:text-slate-900 inline-block transition-scale ease-in delay-75 hover:scale-95'
+      className={`border-2 border-teal-300 py-3 px-8 rounded-tl-lg rounded-br-lg font-bold text-xs uppercase hover:bg-teal-300 inline-block transition-scale ease-in delay-75 hover:scale-95 ${classes}`}
     >
       {label}
     </Link>
