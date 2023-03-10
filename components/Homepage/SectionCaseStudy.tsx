c5fa8e9badeac0798d4221c45d44d976488940db
Murray Williams <murray@waterfalldigital.co.za>
2023-03-10 14:23:15 +0200

    Adjust case study column alignment

--- components/Homepage/SectionCaseStudy.tsx
+++ components/Homepage/SectionCaseStudy.tsx
@@ -5,19 +5,19 @@ import Header from '../Shared/Header'
 export default function SectionCaseStudy() {
   return (
     <section id='case-study'>
-      <div className="flex flex-wrap bg-deep-purple min-h-900 py-24 justify-center p-4 bg-[url('/images/fibre-phoenix-case-study-bird.svg')] bg-no-repeat bg-right">
-        <div className='w-7/12'>
+      <div className="flex flex-wrap bg-deep-purple min-h-900 py-24 justify-center items-center p-4 bg-[url('/images/fibre-phoenix-case-study-bird.svg')] bg-no-repeat bg-right">
+        <div className='w-5/12 flex justify-end'>
           <Image
             src={'/images/fibre-phoenix-case-study.png'}
             width={600}
             height={600}
             alt='Fibre Phoenix Case Study'
-            className='mx-auto'
+            className=''
           />
         </div>
-        <div className='w-3/12 self-start text-left py-8'>
+        <div className='w-4/12 self-start text-left py-8'>
           <Header title='case study' description='Fibre Phoenix' />
-          <p className='text-sm font-light leading-8 text-white py-4 mb-8'>
+          <p className='text-md font-light leading-8 text-white py-4 mb-8'>
             Discover how the Fibre Phoenix web application helped customers find
             the best deals for fibre internet connections. Learn how our web
             agency team created a user-friendly interface, integrated with ISP's
