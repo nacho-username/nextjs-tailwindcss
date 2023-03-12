92b225eda49a3911ee8f59019f397bc4c4da7411
Murray Williams <murray@waterfalldigital.co.za>
2023-03-12 12:45:07 +0200

    Fixes case study mobile width issue

--- components/Homepage/SectionCaseStudy.tsx
+++ components/Homepage/SectionCaseStudy.tsx
@@ -5,8 +5,8 @@ import Header from '../Shared/Header'
 export default function SectionCaseStudy() {
   return (
     <section id='case-study'>
-      <div className="flex flex-wrap bg-deep-purple min-h-900 py-24 justify-center items-center p-4 bg-[url('/images/fibre-phoenix-case-study-bird.svg')] bg-no-repeat bg-right">
-        <div className='w-5/12 flex justify-end'>
+      <div className="flex flex-wrap bg-deep-purple min-h-900 py-12 md:py-24 justify-center items-center p-4 bg-[url('/images/fibre-phoenix-case-study-bird.svg')] bg-no-repeat bg-right">
+        <div className='md:w-5/12'>
           <Image
             src={'/images/fibre-phoenix-case-study.png'}
             width={600}
@@ -15,7 +15,7 @@ export default function SectionCaseStudy() {
             className=''
           />
         </div>
-        <div className='w-4/12 self-start text-left py-8'>
+        <div className='md:w-4/12 self-start text-left py-8'>
           <Header title='case study' description='Fibre Phoenix' />
           <p className='text-md font-light leading-8 text-white py-4 mb-8'>
             Discover how the Fibre Phoenix web application helped customers find
