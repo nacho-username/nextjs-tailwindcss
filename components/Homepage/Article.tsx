6eae851fe6ad7a9213a2ef9f7e2edd9f112d7bba
Murray Williams <murray@waterfalldigital.co.za>
2023-03-12 20:37:13 +0200

    Adds article hover style + icon

--- components/Homepage/Article.tsx
+++ components/Homepage/Article.tsx
@@ -25,17 +25,29 @@ export default function Article({ articleData }: articleDataProps) {
       href={`/blog/${slug}`}
       className='flex flex-col items-start justify-start cursor-pointer'
     >
-      <Image
-        src={`http://localhost:1337${url}`}
-        width={350}
-        height={200}
-        alt=''
-        className='rounded-xl w-full'
-      />
-      <h4 className='text-xs text-gray-400'>
-        By <span className='text-bold text-dark-grey'>Murray Williams</span>
+      <div className='relative group'>
+        <Image
+          src={`http://localhost:1337${url}`}
+          width={350}
+          height={200}
+          alt=''
+          className='rounded-xl w-full'
+        />
+        <div className='opacity-0 group-hover:opacity-100 duration-300 absolute top-0 right-0 bottom-0 left-0 ease-in-out flex justify-center items-center from-deep-purple/70 to-deep-purple/70 bg-gradient-to-t rounded-xl'>
+          <img src='/images/search-icon.svg' alt='' />
+        </div>
+      </div>
+
+      <h4 className='text-sm text-gray-400'>
+        Author:
+        <span className='text-bold text-dark-grey'>Murray Williams</span>
       </h4>
-      <p className='text-xs text-gray-400'>{formatDate(datePosted)}</p>
+      <p className='text-sm text-gray-400'>
+        Posted:
+        <span className='text-bold text-dark-grey'>
+          {formatDate(datePosted)}
+        </span>
+      </p>
       <h2 className='mt-2 text-xl font-bold text-dark-grey leading-6 text-left'>
         {title}
       </h2>
