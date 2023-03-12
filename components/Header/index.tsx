986545d35728298ac3d076dc8c4c42af135ec588
Murray Williams <murray@waterfalldigital.co.za>
2023-03-12 18:33:03 +0200

    Update header to use nav + add bg colour

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,18 +5,20 @@ export default function Header() {
   const scrollDirection = useScrollDirection()
 
   return (
-    <div
-      className={`py-4 px-2 -mb-20 md:sticky ${
-        scrollDirection === 'down' ? 'md:-top-28' : 'md:top-0'
-      } backdrop-blur bg-deep-purple/30 rounded-lg md:mb-0 h-20 container mx-auto transition-all duration-300`}
-    >
-      <Image
-        className='pb-4'
-        src='/images/logo.svg'
-        alt='Logo'
-        width={60}
-        height={100}
-      />
-    </div>
+    <nav className='w-full bg-deep-purple'>
+      <div
+        className={`py-4 px-2 -mb-20 md:sticky ${
+          scrollDirection === 'down' ? 'md:-top-28' : 'md:top-0'
+        } backdrop-blur bg-deep-purple/30 rounded-lg md:mb-0 h-20 container mx-auto transition-all duration-300`}
+      >
+        <Image
+          className='pb-4'
+          src='/images/logo.svg'
+          alt='Logo'
+          width={60}
+          height={100}
+        />
+      </div>
+    </nav>
   )
 }
