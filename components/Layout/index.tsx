10dd6a600658f015e29c72a036787768287efa11
Murray Williams <murray@waterfalldigital.co.za>
2023-03-12 18:33:49 +0200

    Removes page bg colour

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -23,7 +23,7 @@ export default function Layout({
         <meta name='description' content={description} />
         <link rel='icon' href={icon || '/favicon.ico'} />
       </Head>
-      <main className='w-full h-screen bg-deep-purple relative'>
+      <main className='w-full h-screen relative'>
         <Header />
         {children}
         <Footer />
