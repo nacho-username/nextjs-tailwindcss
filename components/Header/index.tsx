7df321f312f0b9f407ce10b28c81ae3a5207f961
Murray Williams <murray@waterfalldigital.co.za>
2023-03-12 19:53:07 +0200

    Adds header logo link

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,24 +1,27 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { useScrollDirection } from '../../utils/scrollHook'
 
 export default function Header() {
   const scrollDirection = useScrollDirection()
 
   return (
-    <nav className='w-full bg-deep-purple'>
-      <div
-        className={`py-4 px-2 -mb-20 md:sticky ${
-          scrollDirection === 'down' ? 'md:-top-28' : 'md:top-0'
-        } backdrop-blur bg-deep-purple/30 rounded-lg md:mb-0 h-20 container mx-auto transition-all duration-300`}
-      >
-        <Image
-          className='pb-4'
-          src='/images/logo.svg'
-          alt='Logo'
-          width={60}
-          height={100}
-        />
-      </div>
+    <nav className='w-full bg-deep-purple cursor-pointer'>
+      <Link href='/'>
+        <div
+          className={`py-4 px-2 -mb-20 md:sticky ${
+            scrollDirection === 'down' ? 'md:-top-28' : 'md:top-0'
+          } backdrop-blur bg-deep-purple/30 rounded-lg md:mb-0 h-20 container mx-auto transition-all duration-300`}
+        >
+          <Image
+            className='pb-4'
+            src='/images/logo.svg'
+            alt='Logo'
+            width={60}
+            height={100}
+          />
+        </div>
+      </Link>
     </nav>
   )
 }
