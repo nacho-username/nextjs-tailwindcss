93705edde1ddf1f0ea4fc9290be9110f2b0d7b89
Murray Williams <murray@waterfalldigital.co.za>
2023-03-12 18:35:05 +0200

    Removes padding

--- components/Shared/BorderButton.tsx
+++ components/Shared/BorderButton.tsx
@@ -17,7 +17,7 @@ export const BorderButton: React.FC<Props> = ({
   return (
     <Link
       href={`/${href}`}
-      className={`border-2 w-48 border-teal-300 py-3 px-8 rounded-tl-lg rounded-br-lg font-bold text-xs uppercase  inline-block relative overflow-hidden group ${classes}`}
+      className={`border-2 w-48 border-teal-300 rounded-tl-lg rounded-br-lg font-bold text-xs uppercase  inline-block relative overflow-hidden group ${classes}`}
     >
       <div className='absolute inset-0 w-0 bg-green transition-all duration-[250ms] ease-out group-hover:w-full'></div>
       <div className='relative group-hover:text-dark-grey text-center'>
