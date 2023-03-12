b5e96eaf9062466e262b36375b48a0274879353e
Murray Williams <murray@waterfalldigital.co.za>
2023-03-12 14:13:47 +0200

    Mobile height adjustments

--- components/Homepage/Hero.tsx
+++ components/Homepage/Hero.tsx
@@ -16,14 +16,14 @@ export default function Hero({
   return (
     <main
       style={{ backgroundImage: `url(http://localhost:1337${heroBgImage})` }}
-      className='bg-deep-purple bg-no-repeat bg-cover bg-center w-full h-[70%] md:h-[90%]'
+      className='bg-deep-purple bg-no-repeat bg-[length:600px_400px] md:bg-cover  md:bg-center w-full h-[90%] mb-12 md:mb-0'
     >
       <div className='container mx-auto flex flex-wrap w-full items-center h-full'>
-        <div className='w-[90%] md:w-[60%] pt-8 md:pt-2 text-white px-4'>
-          <h1 className='text-5xl md:text-7xl font-extrabold capitalize'>
+        <div className='w-[90%] md:w-[60%] pt-28 md:pt-2 text-white px-4'>
+          <h1 className='text-3xl md:text-7xl font-extrabold capitalize'>
             {title}
           </h1>
-          <p className='text-md md:text-xl font-light text-md py-4 pb-12 leading-8 md:leading-10'>
+          <p className='text-sm md:text-xl font-light text-md py-4 pb-12 leading-7 md:leading-10'>
             {description}
           </p>
           <BorderButton
