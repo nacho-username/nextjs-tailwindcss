68e130f5ea2f7295e5311b7e16c581838aa2c51a
Murray Williams <murray@waterfalldigital.co.za>
2023-03-12 14:56:36 +0200

    Add blog cta & style adjustments

--- components/Homepage/Article.tsx
+++ components/Homepage/Article.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { formatDate } from '../../utils/helpers'
+import { SolidButton } from '../Shared/SolidButton'
 
 interface articleProps {
   title: string
@@ -22,20 +23,22 @@ export default function NewsArticle({ articleData }: articleDataProps) {
   return (
     <Link
       href={`/blog/${slug}`}
-      className='flex flex-col items-start justify-start'
+      className='flex flex-col items-start justify-start cursor-pointer'
     >
       <Image
         src={`http://localhost:1337${url}`}
-        width={300}
+        width={350}
         height={200}
         alt=''
-        className='rounded-xl pb-4'
+        className='rounded-xl w-full'
       />
       <h4 className='text-xs text-gray-400'>
         By <span className='text-bold text-dark-grey'>Murray Williams</span>
       </h4>
       <p className='text-xs text-gray-400'>{formatDate(datePosted)}</p>
-      <h2 className='text-xl font-bold text-dark-grey leading-6'>{title}</h2>
+      <h2 className='mt-2 text-xl font-bold text-dark-grey leading-6 text-left'>
+        {title}
+      </h2>
     </Link>
   )
 }
--- components/Homepage/SectionArticleList.tsx
+++ components/Homepage/SectionArticleList.tsx
@@ -1,5 +1,6 @@
 import { title } from 'process'
 import Header from '../Shared/Header'
+import { SolidButton } from '../Shared/SolidButton'
 import NewsArticle from './Article'
 
 interface articleProps {
@@ -16,18 +17,24 @@ interface articleDataProps {
 
 export default function SectionArticleList({ articleData }: articleDataProps) {
   return (
-    <section>
+    <section className='py-8 md:py-16 px-4 text-center'>
       <Header
         title='Stay Updated'
         description='Recent News'
         dark={true}
         alignment='center'
+        classes='text-center'
       />
-      <div className='container mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 pt-8 pb-12'>
+      <div className='container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 pt-8 pb-12'>
         {articleData.map((article: articleProps) => (
           <NewsArticle key={article.slug} articleData={article} />
         ))}
       </div>
+      <SolidButton
+        classes='cursor-pointer bg-dark-grey text-white py-4 px-12'
+        label='All Articles'
+        href='blog'
+      />
     </section>
   )
 }
