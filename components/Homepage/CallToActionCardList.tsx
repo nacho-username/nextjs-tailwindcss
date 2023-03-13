7e8b668a8cc48d6f9bbd10e9d09e4fcf1d8ae981
Murray Williams <murray@waterfalldigital.co.za>
2023-03-13 20:56:37 +0200

    Adjusts top margin

--- components/Homepage/CallToActionCardList.tsx
+++ components/Homepage/CallToActionCardList.tsx
@@ -27,7 +27,7 @@ export default function CallToActionCardList({
 }) {
   return (
     <div className=' py-8 md:py-24 bg-beige'>
-      <div className='flex flex-wrap justify-center gap-4 px-4 md:px-0 md:gap-24 -mt-16 md:-mt-64'>
+      <div className='flex flex-wrap justify-center gap-4 px-4 md:px-0 md:gap-24 -mt-16 md:-mt-48'>
         <CallToActionCard
           backgroundColour={showcaseCardData.backgroundColour}
           headline={showcaseCardData.headline}
