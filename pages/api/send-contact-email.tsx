71c401006b74652ea72a03e4f965b53087891b52
Murray Williams <murray@waterfalldigital.co.za>
2023-03-14 21:41:16 +0200

    Updates postmark to use template

--- pages/api/send-contact-email.tsx
+++ pages/api/send-contact-email.tsx
@@ -6,17 +6,19 @@ const client = new Client(process.env.POSTMARK_API_TOKEN)
 export default async function (req: any, res: any) {
   try {
     const { name, email, phone, message } = req.body
-    const response = await client.sendEmail({
+    const response = await client.sendEmailWithTemplate({
       From: process.env.POSTMARK_FROM_EMAIL,
       To: process.env.POSTMARK_TO_ADDRESS,
       ReplyTo: email,
-      Subject: `Contact Submission from ${name}`,
-      HtmlBody: `<p>You received a contact request from ${name}</p>
-      <ul>
-        <li>Phone: ${phone}</li>
-        <li>Message: ${message}</li>
-      </ul>`,
+      TemplateId: 31073802,
+      TemplateModel: {
+        name,
+        email,
+        phone,
+        message,
+      },
     })
+
     // Return success response
     res.status(200).json({ message: 'Email sent successfully' })
   } catch (error: any) {
