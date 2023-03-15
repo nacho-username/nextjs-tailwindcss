f56fdedb55abf51763c826711749da09e02e11f5
Murray Williams <murray@waterfalldigital.co.za>
2023-03-15 20:37:31 +0200

    Adds bg colour to section

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -16,16 +16,18 @@ export default function index({ articlesData }: BlogProps) {
         description: 'Latest news articles from Waterfall Digital',
       }}
     >
-      <h1 className='container mx-auto text-4xl px-8 md:px-0 mt-8 text-dark-grey'>
-        News Articles
-      </h1>
-      <section className='container mx-auto grid grid-cols-1 md:grid-cols-3 items-start gap-8 md:gap-4 px-8 md:px-0 pt-8 pb-12'>
-        {articlesData.data.map((article: any) => (
-          <div key={article.id} className=' text-white'>
-            <Article article={article} />
-          </div>
-        ))}
-      </section>
+      <div className='bg-white'>
+        <h1 className='container mx-auto text-4xl px-8 md:px-0 mt-8 text-dark-grey'>
+          News Articles
+        </h1>
+        <section className='container mx-auto grid grid-cols-1 md:grid-cols-3 items-start gap-8 md:gap-4 px-8 md:px-0 pt-8 pb-12'>
+          {articlesData.data.map((article: any) => (
+            <div key={article.id} className=' text-white'>
+              <Article article={article} />
+            </div>
+          ))}
+        </section>
+      </div>
     </Layout>
   )
 }
