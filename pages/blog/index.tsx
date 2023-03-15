ffc69ef4b7c85286a34b06d336d5b188c0463dc4
Murray Williams <murray@waterfalldigital.co.za>
2023-03-15 18:05:07 +0200

    Add mobile styles to blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -16,10 +16,10 @@ export default function index({ articlesData }: BlogProps) {
         description: 'Latest news articles from Waterfall Digital',
       }}
     >
-      <h1 className='container mx-auto text-4xl mt-8 text-dark-grey'>
+      <h1 className='container mx-auto text-4xl px-8 md:px-0 mt-8 text-dark-grey'>
         News Articles
       </h1>
-      <section className='container mx-auto grid grid-cols-1 md:grid-cols-3 items-start md:gap-4 pt-8 md:pb-12'>
+      <section className='container mx-auto grid grid-cols-1 md:grid-cols-3 items-start gap-8 md:gap-4 px-8 md:px-0 pt-8 pb-12'>
         {articlesData.data.map((article: any) => (
           <div key={article.id} className=' text-white'>
             <Article article={article} />
