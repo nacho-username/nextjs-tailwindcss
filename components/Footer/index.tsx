f0ed12c241172d80cc1dbcab962d6ba5919d3216
Murray Williams <murray@waterfalldigital.co.za>
2023-03-15 16:52:22 +0200

    Footer mobile adjustments

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,19 +7,17 @@ export default function Footer() {
     <footer
       className={`${
         checkIfContactPage() ? 'bg-dark-grey' : 'bg-deep-purple'
-      } text-white py-12 px-8`}
+      } text-white py-12 px-4 md:px-8`}
     >
       <div className='container mx-auto flex flex-wrap justify-between'>
-        <div className='w-1/3 flex flex-col items-start'>
+        <div className='w-full md:w-1/3 mb-8 md:mb-0 flex flex-col items-start'>
           <img src='/images/logo.svg' alt='Logo' className='w-16 h-16 mb-2' />
           <p className='text-lg font-medium pr-24'>
             Solving unique problems through the use of engaging UX/UI
           </p>
         </div>
         <div className='w-1/3 flex flex-col items-start'>
-          <p className='text-sm mb-2 text-light-purple'>
-            A great meeting location
-          </p>
+          <p className='text-sm mb-2 text-light-purple'>Let's meet</p>
           <p className='text-sm font-light leading-6'>
             <a
               href='https://www.google.com/maps/place/Bootlegger+Coffee+Company/@-33.8903419,18.5080587,17z/data=!3m1!4b1!4m6!3m5!1s0x1dcc5c1ab391d3a1:0xb6182b1af7bd531a!8m2!3d-33.8903419!4d18.5102527!16s%2Fg%2F11c6zxkhfl'
@@ -30,7 +28,9 @@ export default function Footer() {
               Bootleggers - Century City Cape Town
             </a>
           </p>
-          <p className='text-sm mb-2 text-light-purple mt-8'>Contact info</p>
+          <p className='text-sm mb-2 text-light-purple mt-2 md:mt-8'>
+            Contact info
+          </p>
           <p className='text-sm font-light leading-6'>
             <Link
               className='cursor-pointer hover:text-teal-300'
